c2736c7442e957f2ad4e5c982652e91b8365a866
hendrysitorus <hendrysitorus20@gmail.com>
2023-05-05 15:07:48 +0700

    first commit

---
+++ app/views/about/index.php
@@ -0,0 +1,8 @@
+<div class="container">
+    
+<h1 class="mt-4"> About Me</h1>
+<img src="<?=BASEURL  ?>/img/poto profil.jpg" alt="Hendry Sitorus" width="200" >
+
+<p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?=$data['pekerjaan']; ?></p>
+
+</div>
---
+++ app/views/mahasiswa/index.php
@@ -0,0 +1,78 @@
+<div class="container mt-3 ">
+    <div class="row">
+        <div class="col-6">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+            Tambah Data Mahasiswa 
+            </button>
+            <br><br>
+            <h3>Daftar Mahasiswa</h3>
+            <ul class="list-group">
+            <?php foreach ($data['mhs'] as $mhs) : ?>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <?=$mhs ['nama']; ?>
+                    <a href=" <?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id'] ?>" class=" badge badge-primary"> detail </a>
+                </li>
+            <?php endforeach; ?> 
+            </ul>
+
+        </div>
+    </div>
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
+        <form action="<?= BASEURL; ?> /mahasiswa/tambah" method="post" >
+            <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" class="form-control" id="nama" nama="nama">
+             </div>
+
+
+            <div class="form-group">
+                <label for="nrp">NRP</label>
+                <input type="text" class="form-control" id="nrp" name="nrp">
+             </div>
+
+
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" name="email">
+             </div>
+
+
+             <div class="form-group">
+                <label for="jurusan">Jurusan</label>
+                <select class="form-control" id="jurusan" name="jurusan">
+                    <option value= "Teknik Industri">Teknik Industri</option>
+                    <option value= "Manajemen Industri">Manajemen Industri</option>
+                    <option value= "Teknik Mesin">Teknik Mesin</option>
+                    <option value= "Pendidikan Bahasa Inggris">Pendidikan Bahasa Inggris</option>
+                    <option value= "Kesehatan Lingkungan Masyarakat">Kesehatan Lingkungan Masyarakat</option>
+                    <option value= "Pendidikan Bahasa Indonesia">Pendidikan Bahasa Indonesia</option>
+                    <option value= "Ekonomi Pembangunan">Ekonomi Pembangunan</option>
+              
+                </select>
+            </div>
+            
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Tambah Data</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
